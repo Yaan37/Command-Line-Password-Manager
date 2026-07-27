cb09f2bcd1f24905d3208cec9f559a7ecc0ff854
Yaan37 <sufyan2shafiq@gmail.com>
2026-07-27 23:44:51 +0500

    taking username and password from user

--- Main.py
+++ Main.py
@@ -1,1 +1,49 @@
-print("hhelo")
+import getpass
+
+
+
+
+class Password_Manager:
+    def __init__(self):
+        self.user = None
+        self.password = None
+        self.file_name = None 
+        self.history = {}
+
+    def accept_password(self):
+
+        try:
+
+            while True:
+                username = input("Enter a name for this password: ")
+                if username == "":
+                    print("Username canot be empty!")
+                else:
+                    break
+
+            while True:
+                # FOR HIDDING PASWORD GONNA ACTIVATE THIS LATER
+                # passw = getpass.getpass("Please enter your password: ")
+                
+                passw = input("Please enter your password: ")                               
+                if passw == "":
+                    print("Password canot be empty!")
+                else:
+                    break
+
+            self.history[username] = passw
+                
+        except Exception as e:
+            print(f"Error: {e}")
+
+
+
+
+
+            
+
+
+if __name__ == "__main__":
+
+    pm = Password_Manager()
+    pm.accept_password()
